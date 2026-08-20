f1ab2e5b32b25887cc5970067358bf8d92414697
Tom <tfyoder1@users.noreply.github.com>
2026-08-20 16:46:54 -0400

    Move sleeping quarters back wall

--- src/server/RoomBuilder.lua
+++ src/server/RoomBuilder.lua
@@ -2783,7 +2783,7 @@ local function makeSleepingQuartersRoom(roomFolder)
 	local wallSegmentDepth = (depth - branchGapDepth) / 2
 	local wallSegmentCenterZ = (branchGapDepth / 2 + depth / 2) / 2
 	createPart(room, "SleepingRightWallFront", Vector3.new(1, height, wallSegmentDepth), cframeAt(origin, width / 2, height / 2, wallSegmentCenterZ), Color3.fromRGB(103, 84, 92), Enum.Material.SmoothPlastic)
-	createPart(room, "SleepingRightWallBack", Vector3.new(1, height, wallSegmentDepth), cframeAt(origin, width / 2, height / 2, -wallSegmentCenterZ), Color3.fromRGB(103, 84, 92), Enum.Material.SmoothPlastic)
+	createPart(room, "SleepingRightWallBack", Vector3.new(1, height, wallSegmentDepth), cframeAt(origin, width / 2, height / 2, -wallSegmentCenterZ + 5.5), Color3.fromRGB(103, 84, 92), Enum.Material.SmoothPlastic)
 
 	createSpawnLocation(room, "SleepingQuartersSpawn", "SleepingQuarters", SLEEPING_QUARTERS_SPAWN_CFRAME, Color3.fromRGB(255, 142, 191), false)
 
